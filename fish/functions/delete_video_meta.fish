1a0097b8f5c33ec154a396aec4a713a6f4fcfefc
songchenwen <sanvinsung@gmail.com>
2015-05-23 16:46:41 +0800

    Auto Install Maid Launch Agent

--- fish/functions/delete_video_meta.fish
+++ fish/functions/delete_video_meta.fish
@@ -1,5 +1,5 @@
 function delete_video_meta --argument-name INPUT -d "Delete Metadata of a Video File"
-	set -l OUTPUT "NewWithoutMeta$INPUT"
+	set -l OUTPUT (printf '%s/NewWithoutMeta%s' (dirname $INPUT) (basename $INPUT))
 	ffmpeg -loglevel error -i $INPUT -c copy -map_metadata -1 $OUTPUT
 	if test $status -eq 0
 		rm $INPUT
--- maid_rules/install.sh
+++ maid_rules/install.sh
@@ -11,3 +11,20 @@ echo "Maid configuring rules"
 
 cd ~/.config
 ln -Fs "$MAIDDIR/rules.rb" ~/.maid/rules.rb
+
+RUNSH="$MAIDDIR/run.sh"
+if [ ! -e "$RUNSH" ]; then
+   	echo "Maid generating run.sh"
+   	PATHTORUBY=$(which ruby)
+   	PATHTOMAID=$(which maid)
+   	cat "$MAIDDIR/run.sh.temp" | sed "s|PATHTORUBY|$PATHTORUBY|" | sed "s|PATHTOMAID|$PATHTOMAID|" > $RUNSH
+   	chmod +x $RUNSH
+fi
+
+launchctl list | grep -q -E 'com.songchenwen.maid'
+if [[ $? != 0 ]]; then
+	echo "Maid adding launch agent"
+
+	cat "$MAIDDIR/com.songchenwen.maid.plist.temp" | sed "s|PATHTO|$MAIDDIR|" > ~/Library/LaunchAgents/com.songchenwen.maid.plist
+	launchctl load ~/Library/LaunchAgents/com.songchenwen.maid.plist
+fi
