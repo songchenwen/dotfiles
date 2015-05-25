faa4a06e7d6b57e7679afa1891af7349bffbaf6d
songchenwen <sanvinsung@gmail.com>
2015-05-25 15:16:03 +0800

    Add OSD Message for Seeking

--- mpv/scripts/autosub.lua
+++ mpv/scripts/autosub.lua
@@ -3,11 +3,13 @@
 -- keyname script_binding auto_load_subs
 local utils = require 'mp.utils'
 function load_sub_fn()
-    subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
+    subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path 
+    local path = mp.get_property("path")
+    path = string.sub(path, 0, -2 - string.len(get_extension(path)))
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
     t = {}
-    t.args = {subl, "-q", "-s", "-l", "en", "--", mp.get_property("path")}
+    t.args = {subl, "-q", "-s", "-l", "en", "--", path}
     res = utils.subprocess(t)
     if res.status == 0 then
         mp.commandv("rescan_external_files", "reselect") 
@@ -17,6 +19,10 @@ function load_sub_fn()
         mp.msg.warn("Subtitle download failed")
         mp.osd_message("Subtitle download failed")
     end
+end
+
+function get_extension(path)
+    return string.match(path, "%.([^%.]+)$" )
 end
  
 mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
