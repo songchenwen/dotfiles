4a8235723ceea8a1b64e6d886b74d0aa531c173c
songchenwen <me@songchenwen.com>
2016-01-09 14:53:55 +0800

    Update QQ

--- apps/install_apps.sh
+++ apps/install_apps.sh
@@ -4,11 +4,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 urls=("https://pqrs.org/osx/karabiner/files/Karabiner-10.6.0.dmg"
 	  "https://pqrs.org/osx/karabiner/files/Seil-11.2.0.dmg"
-	  "https://iterm2.com/downloads/stable/iTerm2_v2_0.zip"
-	  "https://github.com/Hammerspoon/hammerspoon/releases/download/0.9.31/Hammerspoon-0.9.31.zip"
-	  "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.2.dmg"
+	  "https://iterm2.com/downloads/stable/iTerm2-2_1_4.zip"
+	  "https://github.com/Hammerspoon/hammerspoon/releases/download/0.9.42/Hammerspoon-0.9.42.zip"
+	  "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.0.5.5.dmg"
 	  "https://github.com/ohdarling/GoAgentX/releases/download/rel-v2.3.7/GoAgentX-v2.3.7.dmg"
-	  "http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.2.dmg")
+	  "http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.6.dmg")
 
 names=("Karabiner"
 	   "Seil"
